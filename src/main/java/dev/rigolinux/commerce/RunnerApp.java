5ded9ddb8ba3b4d354adb9eea978f730f0fc110e
rigolinux <rigoberto7567@gmail.com>
2024-10-18 18:29:05 -0600

    testing clases in packages

--- src/main/java/dev/rigolinux/commerce/RunnerApp.java
+++ src/main/java/dev/rigolinux/commerce/RunnerApp.java
@@ -1,13 +1,21 @@
 package dev.rigolinux.commerce;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class RunnerApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RunnerApp.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(RunnerApp.class, args);
+
+		WelcomeMessage welcomeMessage = (WelcomeMessage) context.getBean("WelcomeMessage");
+		System.out.println(welcomeMessage.getWelcomeMessage());
+		//this is a comment
+		//var message = new dev.rigolinux.commerce.WelcomeMessage();
+		//System.out.println(message.getWelcomeMessage());
 	}
 
 }
--- src/main/java/dev/rigolinux/commerce/WelcomeMessage.java
+++ src/main/java/dev/rigolinux/commerce/WelcomeMessage.java
@@ -1,7 +1,12 @@
-package foo.bar;
+package dev.rigolinux.commerce;
 
+import org.springframework.stereotype.Component;
+
+@Component("WelcomeMessage")
 public class WelcomeMessage {
     public String getWelcomeMessage(){
         return "Welcome To My Spring Application";
     }
+
+
 }
