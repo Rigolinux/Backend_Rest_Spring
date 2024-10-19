a21539446440c3ff03f52a1f618431ab9a2cb89d
rigolinux <rigoberto7567@gmail.com>
2024-10-18 18:38:03 -0600

    configure logs

--- src/main/java/dev/rigolinux/commerce/RunnerApp.java
+++ src/main/java/dev/rigolinux/commerce/RunnerApp.java
@@ -1,18 +1,25 @@
 package dev.rigolinux.commerce;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+
+
 @SpringBootApplication
 public class RunnerApp {
 
+	private static final Logger log = LoggerFactory.getLogger(RunnerApp.class);
+
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(RunnerApp.class, args);
+		log.info("App Should go");
 
-		WelcomeMessage welcomeMessage = (WelcomeMessage) context.getBean("WelcomeMessage");
-		System.out.println(welcomeMessage.getWelcomeMessage());
+		//WelcomeMessage welcomeMessage = (WelcomeMessage) context.getBean("WelcomeMessage");
+		//System.out.println(welcomeMessage.getWelcomeMessage());
 		//this is a comment
 		//var message = new dev.rigolinux.commerce.WelcomeMessage();
 		//System.out.println(message.getWelcomeMessage());
