994eff8de17910a70f71638a8cc9a723042d727b
rigolinux <rigoberto7567@gmail.com>
2024-10-20 14:54:32 -0600

    update crud controller

--- src/main/java/dev/rigolinux/commerce/run/RunController.java
+++ src/main/java/dev/rigolinux/commerce/run/RunController.java
@@ -1,13 +1,16 @@
 package dev.rigolinux.commerce.run;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
+@RequestMapping("/api/runs")
 public class RunController {
 
     private final RunRepository runRepository;
@@ -22,11 +25,41 @@ public class RunController {
     }
 
 
-    @GetMapping("/runs")
+    @GetMapping()
     List<Run> runs() {
         return runRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    Run getRunById(@PathVariable Integer id) {
 
+        Optional<Run> run = runRepository.findById(id);
+        if (run.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Run not found");
+        }
+
+        return run.get();
+    }
+
+    //Post new run with all fields from body and method post
+    @PostMapping()
+    Run newRun(@RequestBody Run newRun) {
+        return runRepository.save(newRun);
+    }
+
+
+    @PutMapping("/{id}")
+    Run replaceRun(@RequestBody Run newRun, @PathVariable Integer id) {
+        Optional<Run> run = runRepository.update(newRun, id);
+        if (run.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Run not found");
+        }
+        return run.get();
+    }
+
+    @DeleteMapping("/{id}")
+    void deleteRun(@PathVariable Integer id) {
+        runRepository.delete(id);
+    }
 
 }
--- src/main/java/dev/rigolinux/commerce/run/RunRepository.java
+++ src/main/java/dev/rigolinux/commerce/run/RunRepository.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Repository;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Repository
 public class RunRepository {
@@ -20,4 +22,66 @@ public class RunRepository {
         runs.add(new Run(3,"Night Run", LocalDateTime.now(),LocalDateTime.now().plusHours(3), 15,Location.INDOOR));
         runs.add(new Run(4,"Morning Run", LocalDateTime.now(),LocalDateTime.now().plusHours(1), 5,Location.INDOOR));
     }
+
+    Optional<Run> findById(Integer id) {
+        return runs.stream().filter(run -> run.id() == id).findFirst();
+    }
+
+    Run save(Run run) {
+        runs.add(run);
+        return run;
+    }
+
+
+    Optional<Run> delete(int id) {
+
+        Optional<Run> runToDeleteOptional = runs.stream()
+                .filter(r -> r.id() == id)
+                .findFirst();
+
+
+        if (runToDeleteOptional.isEmpty()) {
+            return Optional.empty();
+        }
+
+
+        Run runToDelete = runToDeleteOptional.get();
+        runs.remove(runToDelete);
+        return Optional.of(runToDelete);
+    }
+
+    Optional<Run> update(Run run, int id) {
+
+        Optional<Run> runToUpdateOptional = runs.stream()
+                .filter(r -> r.id() == id)
+                .findFirst();
+
+
+        if (runToUpdateOptional.isEmpty()) {
+            return Optional.empty();
+        }
+
+
+        Run runToUpdate = runToUpdateOptional.get();
+
+        Run updatedRun = new Run(
+                runToUpdate.id(),
+                run.title(),
+                run.startedOn(),
+                run.completedOn(),
+                run.miles(),
+                run.location()
+        );
+
+
+        runs.remove(runToUpdate);
+        runs.add(updatedRun);
+
+
+        return Optional.of(updatedRun);
+    }
+
+
+
 }
+
